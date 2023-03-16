711781479bda16060e33dcbdbb29d73129891555
Камран <kem@192.168.1.23>
2023-03-16 20:25:33 +0300

    update

--- gendiff/function_tools/file_difference.py
+++ gendiff/function_tools/file_difference.py
@@ -1,4 +1,8 @@
 def get_diff(old_dict, new_dict):
+    '''
+    Make new updated dictionary with difference block
+    based on given dicts
+    '''
 
     def get_type(old_value, new_value):
         if old_value == new_value:
@@ -23,14 +27,13 @@ def get_diff(old_dict, new_dict):
         key: get_type(old_dict[key], new_dict[key])
         for key in old_dict.keys() & new_dict.keys()
     })
-    removed_val = {
-        key: {'type': 'removed', 'value': old_dict[key]}
-        for key in old_dict.keys() - new_dict.keys()
-    }
-    added_val = {
-        key: {'type': 'added', 'value': new_dict[key]}
-        for key in new_dict.keys() - old_dict.keys()
-    }
-    data_diff.update(removed_val)
-    data_diff.update(added_val)
+    removed_keys = old_dict.keys() - new_dict.keys()
+    added_keys = new_dict.keys() - old_dict.keys()
+
+    def update_data_diff(keys, status, given_dict):
+        for key in keys:
+            data_diff[key] = {'type': status, 'value': given_dict[key]}
+
+    update_data_diff(removed_keys, 'removed', old_dict)
+    update_data_diff(added_keys, 'added', new_dict)
     return {key: data_diff[key] for key in sorted(data_diff.keys())}
